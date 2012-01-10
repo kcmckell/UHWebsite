23d214c3d3e88500a8fba36fedcc31df1740f898
Clay McKell <kcmckell@gmail.com>
2012-01-09 22:24:13 -1000

    Common head matter placed in commonhead.php.
    Moved most (all) common HEAD elements in the file commonhead.php and REQUIRED it in all pages.

--- public_html/index.php
+++ public_html/index.php
@@ -5,59 +5,10 @@
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 
 <head>
-    <base href="http://localhost:8888/UHWebsite/public_html/" />
-<meta charset="utf-8">
-
-  <!-- Use the .htaccess and remove these lines to avoid edge case issues.
-       More info: h5bp.com/b/378 -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
-  <title>K. Clay McKell at the University of Hawai&#699i at M&#257noa</title>
-  <meta name="description" content="Online webspace for the academic escapades of K. Clay McKell at the University of Hawaii at Manoa.">
-  <meta name="author" content="K. Clay McKell">
-
-<!-- Mobile viewport optimized: j.mp/bplateviewport -->
-<meta name="viewport" content="width=device-width,initial-scale=1">
-
-<link rel="shortcut icon" href="img/favicon.ico"  type="image/x-icon" />
-<meta name="keywords" content="University of Hawaii, Manoa, Honolulu, Electrical Engineering, PhD, graduate, student, control, game, theory">
-
-
-  <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
-
-
-<!-- CSS-->
-<link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow|Cuprum' rel='stylesheet' type='text/css'>
-
-<link rel="stylesheet" href="css/normalize.css">
-<link rel="stylesheet" href="css/grid.css">
-<link rel="stylesheet" href="css/superfish.css">
-<link rel="stylesheet" href="css/flexslider.css" />
-<link rel="stylesheet" href="css/style.css">
-<link rel="stylesheet" href="css/custom.css"/>
-<!-- end CSS-->
     
-<!-- JS-->
-<!-- grab jQuery from Google SDN , fallback on local, if offline -->
-<script type="text/javascript" src="http://www.google.com/jsapi"></script>
-<script type="text/javascript">google.load("jquery", "1.6.2");</script>
-<script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
-<!-- modernizr library for IE 6-8 -->
-<!--[if lt IE 9]><script src="js/libs/modernizr-2.0.6.min.js"></script><![endif]-->
-
-<!-- superfish menu by Joel Birch http://users.tpg.com.au/j_birch/plugins/superfish/ -->
-<script type="text/javascript" src="js/superfish/hoverIntent.js"></script>
-<script type="text/javascript" src="js/superfish/superfish.js"></script>
-
-<!-- responsive select menu by Matt Kersley. Turns ul / ol into select box https://github.com/mattkersley/Responsive-Menu -->
-<script type="text/javascript" src="js/jquery.mobileselect.js"></script>
-
-<!-- responsive FlexSlider slideshow by (C) http://flex.madebymufffin.com/ -->
-<script src="js/jquery.flexslider-min.js"></script>
-
-<!-- Shiv for HTML5 details tag in browsers other than Chrome: https://github.com/manuelbieh/Details-Polyfill -->
-<script src="js/mylibs/details.polyfill.min.js"></script>
-<!-- end JS-->
+    <?php require('commonhead.php'); ?>
+  <title>K. Clay McKell at the University of Hawai&#699i at M&#257noa</title>
+  
 </head>
 
 <body onLoad="setTimeout(function() {window.scrollTo(0, 1)}, 100)" id="home">
