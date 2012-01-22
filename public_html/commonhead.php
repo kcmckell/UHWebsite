88e8571d2dfaffbdbae8649335cb0acc699e1ae1
Clay McKell on Silver Bullet <kcmckell@gmail.com>
2012-01-22 13:10:40 -1000

    Fix bibtexbrowser's call to old version of PHP. [nm]

--- public_html/commonhead.php
+++ public_html/commonhead.php
@@ -30,8 +30,13 @@
     
 <!-- JS-->
 <!-- grab jQuery from Google SDN , fallback on local, if offline -->
-<script type="text/javascript" src="http://www.google.com/jsapi"></script>
-<script type="text/javascript">google.load("jquery", "1.6.2");</script>
+
+<script type="text/javascript" src="https://www.google.com/jsapi?key=ABQIAAAAyKn8HaXYbbw-mn2gmwFcRRShAtsdeGT-UGtm-dQfxBjmpMYAexTFEqIuhdDi8zEBagigNPSTab7PQg"></script>
+<!--Call jQuery with permissions from above-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<!--This method came stock with YAMB to access Google CDN's jQuery library.  The above should work just fine.-->
+    <!--<script type="text/javascript" src="http://www.google.com/jsapi"></script>
+    <script type="text/javascript">google.load("jquery", "1.6.2");</script>-->
 <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
 <!-- modernizr library for IE 6-8 -->
 <!--[if lt IE 9]><script src="js/libs/modernizr-2.0.6.min.js"></script><![endif]-->
